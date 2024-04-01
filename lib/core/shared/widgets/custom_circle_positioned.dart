46fc74ab92744d6b4b5670935bc1c593c6f5de17
Abdelrhman Elbattawy <abdo.elbattawy1st@gmail.com>
2024-04-01 17:38:44 +0200

    added circle positioned to language view

---
+++ lib/core/shared/widgets/custom_circle_positioned.dart
@@ -0,0 +1,26 @@
+import 'package:ecommerce_app/core/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomCirclePositioned extends StatelessWidget {
+  const CustomCirclePositioned({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: -100,
+      top: -120,
+      child: Container(
+        height: MediaQuery.of(context).size.width * .6,
+        width: MediaQuery.of(context).size.width * .6,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(
+            MediaQuery.of(context).size.width * .6,
+          ),
+          color: AppColors.primaryForegroundColor,
+        ),
+      ),
+    );
+  }
+}
--- lib/features/language/views/widgets/language_view_body.dart
+++ lib/features/language/views/widgets/language_view_body.dart
@@ -2,6 +2,7 @@ import 'package:ecommerce_app/core/constants/app_routes.dart';
 import 'package:ecommerce_app/core/constants/app_text_style.dart';
 import 'package:ecommerce_app/core/constants/app_tranlsations_keys.dart';
 import 'package:ecommerce_app/core/shared/widgets/custom_button.dart';
+import 'package:ecommerce_app/core/shared/widgets/custom_circle_positioned.dart';
 import 'package:ecommerce_app/features/language/controller/language_controller.dart';
 import 'package:ecommerce_app/features/language/views/widgets/language_list.dart';
 import 'package:flutter/cupertino.dart';
@@ -14,25 +15,30 @@ class LanguageViewBody extends GetView<LanguageControllerImpl> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            AppTranslationsKeys.languageTitle.tr,
-            style: AppTextStyle.textStyle20,
+    return Stack(
+      children: [
+        const CustomCirclePositioned(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                AppTranslationsKeys.languageTitle.tr,
+                style: AppTextStyle.textStyle20,
+              ),
+              const SizedBox(height: 15),
+              const LanguageList(),
+              const SizedBox(height: 20),
+              CustomButton(
+                title: AppTranslationsKeys.languageButtonText.tr,
+                horizontalPadding: 24,
+                onPressed: () => Get.toNamed(AppRoutes.onBoarding),
+              ),
+            ],
           ),
-          const SizedBox(height: 15),
-          const LanguageList(),
-          const SizedBox(height: 20),
-          CustomButton(
-            title: AppTranslationsKeys.languageButtonText.tr,
-            horizontalPadding: 24,
-            onPressed: () => Get.toNamed(AppRoutes.onBoarding),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
