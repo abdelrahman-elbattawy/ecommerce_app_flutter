a542b20b8723d9e1f4e6293b37bbbc3e7665e6d7
Abdelrhman Elbattawy <abdo.elbattawy1st@gmail.com>
2024-04-21 10:03:28 +0200

    remove search from favorite view

--- lib/features/favorite/controller/myfavorite_controller.dart
+++ lib/features/favorite/controller/myfavorite_controller.dart
@@ -1,21 +1,17 @@
 import 'package:ecommerce_app/core/shared/data/models/item_model.dart';
 import 'package:ecommerce_app/features/favorite/controller/favorite_controller.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 abstract class MyFavoriteController extends GetxController {
-  void intialTextEditingController();
   void intialItemsList();
   void setFavorite(ItemModel itemModel);
 }
 
 class MyFavoriteControllerImpl extends MyFavoriteController {
   List<ItemModel> itemsList = [];
-  late TextEditingController searchController;
 
   @override
   void onInit() {
-    intialTextEditingController();
     intialItemsList();
 
     super.onInit();
@@ -24,11 +20,6 @@ class MyFavoriteControllerImpl extends MyFavoriteController {
   @override
   void intialItemsList() {
     itemsList = Get.find<FavoriteControllerImpl>().favoritesList;
-  }
-
-  @override
-  void intialTextEditingController() {
-    searchController = TextEditingController();
   }
 
   @override
--- lib/features/favorite/views/widgets/myfavorite_view_body.dart
+++ lib/features/favorite/views/widgets/myfavorite_view_body.dart
@@ -1,7 +1,6 @@
 import 'package:ecommerce_app/core/constants/app_text_style.dart';
 import 'package:ecommerce_app/core/constants/app_tranlsations_keys.dart';
 import 'package:ecommerce_app/core/functions/goto_item_details.dart';
-import 'package:ecommerce_app/core/shared/widgets/custom_text_field.dart';
 import 'package:ecommerce_app/features/favorite/controller/myfavorite_controller.dart';
 import 'package:ecommerce_app/features/home/views/widgets/item_details/item_card.dart';
 import 'package:flutter/material.dart';
@@ -19,18 +18,6 @@ class MyFavoriteViewBody extends GetView<MyFavoriteControllerImpl> {
           AppTranslationsKeys.homeTabBarFavorite.tr,
           style: AppTextStyle.textStyle20,
           textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: 20),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: CustomTextField(
-            controller: controller.searchController,
-            hintText: AppTranslationsKeys.homeViewSeachHint.tr,
-            prefixIcon: Icons.search_outlined,
-            suffixIcon: Icons.cancel,
-            onPrefixIcon: () {},
-            onSuffixIcon: () => controller.searchController.clear(),
-          ),
         ),
         const SizedBox(height: 10),
         Padding(
